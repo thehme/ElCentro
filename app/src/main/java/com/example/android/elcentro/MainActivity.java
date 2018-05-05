367237ae40584cfc3a5d68320629f1e49305e82a
thehme <bcortez23@gmail.com>
2018-05-04 23:26:09 -0400

    added intent to make call when phone icon or phone number is clicked and also intent to open website when globe is clicked

--- app/src/main/java/com/example/android/elcentro/MainActivity.java
+++ app/src/main/java/com/example/android/elcentro/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.android.elcentro;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout makeCall = findViewById(R.id.phone_number);
+
+        makeCall.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:6093942056"));
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+    }
+    public void launchWebsite(View view) {
+        String url = "https://www.catholiccharitiestrenton.org/locations/el-centro";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
